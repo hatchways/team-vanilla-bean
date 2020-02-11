d9986544522e7e7f5909f78dcad35b4715d5a9fc
Kulraj <kulraj.dhanjal@gmail.com>
2020-02-11 16:03:58 -0500

    setting up basic routes and experimenting with mongodb

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const pingRouter = require("./routes/ping");
 const { json, urlencoded } = express;
 
 const app = express();
-require("./database");
+let testModel = require("./database");
 
 app.use(logger("dev"));
 app.use(json());
@@ -20,6 +20,79 @@ app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
+
+
+// Example model
+
+// { username: "Bob",
+//  new_user: "False",
+//  columns: [
+//             {col_title: "philo",
+//                 cards: ["card1", "card2"]
+//         }
+//       ]
+//     }
+
+
+//delete table on startup for testing
+testModel.deleteMany({}, function(err,removed) {
+
+});
+
+
+//hardcoded for testing
+let msg = new testModel({
+  "username": "kulraj",
+  "cols": [{"colTitle" : "math", "cards" : ["study"]}]
+})
+ 
+msg.save()
+   .then(doc => {
+     console.log(doc)
+   })
+   .catch(err => {
+     console.error(err)
+   })
+
+testModel
+.find({})
+.then(doc => {
+  console.log(doc)
+})
+.catch(err => {
+  console.error(err)
+})
+
+
+app.post("/newUserBoard", (req,res) =>{
+  // authenticate user
+  const username =  req.body.username;
+  let userModel = new testModel({
+    "username": username,
+    "cols": [{"colTitle" : "in progress", "cards" : []},
+            {"colTitle" : "completed", "cards" : []} ]
+  })
+  res.json({ "temp": "temp"})
+
+})
+
+
+//TODO
+app.post("/newColumn", (req,res) =>{
+  // authenticate user
+  //is username already known at this point?
+  const username =  req.body.username;
+
+
+  let userModel = new testModel({
+    "username": username,
+    "cols": [{"colTitle" : "in progress", "cards" : []},
+            {"colTitle" : "completed", "cards" : []} ]
+  })
+  res.json({ "temp": "temp"})
+
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
