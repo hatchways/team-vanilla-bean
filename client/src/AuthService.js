26385dfac45e1dd958164385655fab47d1d864e2
Zeinab Farag <zzeinabfaragg@gmail.com>
2020-02-13 13:11:58 -0500

    remove comments from auth

--- client/src/AuthService.js
+++ client/src/AuthService.js
@@ -1,13 +1,11 @@
 import decode from "jwt-decode";
 export default class AuthService {
-  // Initializing important variables
   constructor() {
-    this.fetch = this.fetch.bind(this); // React binding stuff
+    this.fetch = this.fetch.bind(this);
     this.login = this.login.bind(this);
   }
 
   login(route, email, password) {
-    // Get a token from api server using the fetch api
     return this.fetch(`/user/${route}`, {
       method: "POST",
       body: JSON.stringify({
@@ -15,22 +13,20 @@ export default class AuthService {
         password
       })
     }).then(res => {
-      this.setStorage(res.token, email); // Setting the token in localStorage
+      this.setStorage(res.token, email);
       return Promise.resolve(res);
     });
   }
 
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken(); // GEtting token from localstorage
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
 
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
       if (decoded.exp < Date.now() / 1000) {
-        // Checking if token is expired. N
         return true;
       } else return false;
     } catch (err) {
@@ -39,30 +35,25 @@ export default class AuthService {
   }
 
   setStorage(token, email) {
-    // Saves user token to localStorage
     localStorage.setItem("token", token);
     localStorage.setItem("email", email);
   }
 
   getToken() {
-    // Retrieves the user token from localStorage
     return localStorage.getItem("token");
   }
 
   logout() {
-    // Clear user token and profile data from localStorage
     localStorage.removeItem("token");
   }
 
   fetch(url, options) {
-    // performs api calls sending the required authentication headers
     const headers = {
       Accept: "application/json",
       "Content-Type": "application/json"
     };
 
     // Setting Authorization header
-    // Authorization: Bearer xxxxxxx.xxxxxxxx.xxxxxx
     if (this.loggedIn()) {
       headers["Authorization"] = "Bearer " + this.getToken();
     }
@@ -76,9 +67,7 @@ export default class AuthService {
   }
 
   _checkStatus(response) {
-    // raises an error in case response status is not a success
     if (response.status >= 200 && response.status < 300) {
-      // Success status lies between 200 to 300
       return response;
     } else {
       var error = new Error(response.statusText);
