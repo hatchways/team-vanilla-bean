cf6189a2ca6f11a2355329798ecf00eb1f12c27d
Zeinab Farag <zzeinabfaragg@gmail.com>
2020-02-18 06:38:49 -0500

    auth validation

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   password: {
     type: String,
@@ -12,4 +11,4 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = ("User", UserSchema);
