4a619958db9746acd56cc7203487073238d45ab5
milkcan <milkcanbass@gmail.com>
2020-02-18 12:12:00 -0500

    make sure the string works or not

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect("mongodb://localhost/kanban", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
