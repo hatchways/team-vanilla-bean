2fa6ab676bf20b12c0f41c00613447aa1cb95211
Zeinab Farag <zzeinabfaragg@gmail.com>
2020-02-20 21:17:59 -0500

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
-import DashBoard from "./pages/DashbBoard";
+import DashBoard from "./pages/DashBoard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./utils/routes";
