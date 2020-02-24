8cdabd46d033071f464fe76696072c4b82055a84
Zeinab Farag <zzeinabfaragg@gmail.com>
2020-02-24 09:34:48 -0500

    dashboard

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -1,20 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import CreateBoard from "../components/CreateBoard";
-
-const DashBoard = () => {
-  return (
-    <div>
-      <CreateBoard />
-      <h1>Dashboard</h1>
-    </div>
-  );
-=======
 import NewColumnArea from "../components/ColumnArea";
 
 const DashBoard = () => {
   return <NewColumnArea />;
->>>>>>> new-drag-drop
 };
 
 export default DashBoard;
