ebfffd86e77ff6b47689937681a3586c25dfa5d0
Zeinab Farag <zzeinabfaragg@gmail.com>
2020-02-24 00:49:49 -0500

    signin

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from "react";
-import {
-  Container,
-  Button,
-  TextField,
-  Grid,
-  Typography
-} from "@material-ui/core";
+import { Container, TextField, Grid, Typography } from "@material-ui/core";
+import Button from "../components/BlueButton";
 import useStyles from "../themes/AuthStyles";
 import { Link } from "react-router-dom";
 import { login, loggedIn } from "../AuthService";
@@ -54,12 +49,6 @@ const SignIn = props => {
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                InputLabelProps={{
-                  classes: {
-                    root: classes.inputLabel,
-                    shrink: classes.shrink
-                  }
-                }}
                 InputProps={{
                   classes: {
                     root: classes.input,
@@ -80,12 +69,6 @@ const SignIn = props => {
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                InputLabelProps={{
-                  classes: {
-                    root: classes.inputLabel,
-                    shrink: classes.shrink
-                  }
-                }}
                 InputProps={{
                   classes: {
                     root: classes.input,
@@ -99,25 +82,17 @@ const SignIn = props => {
                   }
                 }}
               />
-              <Button
-                className={classes.button}
-                type="submit"
-                variant="contained"
-                color="primary"
-                size="large"
-              >
-                Login
-              </Button>
+              <Button>Login</Button>
             </form>
           </div>
         </Container>
 
         <Container className={classes.footer}>
-          <Typography paragraph variant="h2">
+          <Typography paragraph variant="h3">
             Don't have an account?
           </Typography>
 
-          <Typography variant="h2">
+          <Typography variant="h3">
             <Link to="/signup">Create</Link>
           </Typography>
         </Container>
