dede62bf94f249cb892e5358804b8b602a12c732
Zeinab Farag <zzeinabfaragg@gmail.com>
2020-02-29 16:44:26 -0500

    theme

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -10,11 +10,52 @@ export const theme = createMuiTheme({
       fontWeight: 500
     },
     h2: {
-      fontSize: 15,
-      fontWeight: 500
+      fontSize: 18,
+      fontWeight: 600
+    },
+    h3: {
+      fontSize: 14,
+      fontWeight: 600
     }
   },
   palette: {
-    primary: { main: "#759CFC" }
+    primary: { main: "#759CFC" },
+    secondary: { main: "#B5C0D9" }
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        fontWeight: 600
+      }
+    },
+    MuiInputLabel: {
+      outlined: {
+        color: "black",
+        textAlign: "center",
+        "&$shrink": {
+          width: "auto",
+          color: "#759CFC"
+        },
+        width: "90%"
+      }
+    },
+    MuiOutlinedInput: {
+      notchedOutline: {
+        borderColor: "rgba(117,156,252,0.3)"
+      },
+      root: {
+        fontWeight: 500,
+        "&:hover $notchedOutline": {
+          borderColor: "#759CFC"
+        }
+      }
+    },
+    MuiInput: {
+      underline: {
+        "&&&:before": {
+          borderBottom: "none"
+        }
+      }
+    }
   }
 });
