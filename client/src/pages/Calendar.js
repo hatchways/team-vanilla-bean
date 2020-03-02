cbb79b1f2aacdb77cd0406f7578fc739002bdac8
Zeinab Farag <zzeinabfaragg@gmail.com>
2020-03-01 19:12:18 -0500

    component

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Calendar = () => {
-  return <h1> Calendar</h1>;
+const CardsCalendar = () => {
+  return <div></div>;
 };
 
-export default Calendar;
+export default CardsCalendar;
