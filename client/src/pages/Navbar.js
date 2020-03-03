ac19660c569635d7c5ee04d86cd525a7a329165a
Zeinab Farag <zeinabfarag@Zeinabs-MacBook-Pro.local>
2020-03-03 00:46:17 -0500

    fix modal error

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -2,12 +2,14 @@ import React from "react";
 import BlueNav from "../components/BlueNav";
 import TopNav from "../components/TopNav";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import CardModal from "../components/CreateCard/Modal";
 
 const Navbar = () => {
   return (
     <CssBaseline>
       <TopNav />
       <BlueNav />
+      <CardModal />
     </CssBaseline>
   );
 };
