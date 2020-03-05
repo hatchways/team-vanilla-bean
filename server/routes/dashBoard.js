3356dc8b788b92aa5f559c1f13582384892fc2bc
Zeinab Farag <zeinabfarag@Zeinabs-MacBook-Pro.local>
2020-03-05 18:39:26 -0500

    remove comment

--- server/routes/dashBoard.js
+++ server/routes/dashBoard.js
@@ -8,24 +8,6 @@ const { Task, Column, Dashboard } = require("../models/Dashboard");
 const Calendar = require("../models/Calendar");
 const User = require("../models/User");
 
-//@CreateBoard
-// router.get("/", checkToken, async (req, res) => {
-//   let userId = req.decoded.id;
-
-//   try {
-//     let result = await Dashboard.findOne({ user: userId });
-
-//     if (result) {
-//       res.status(200).json({ result });
-//     } else {
-//       res.status(404).json({ error: "no dashboard" });
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     res.status(404).json({ error: "Failed to get dashboard" });
-//   }
-// });
-
 //Retrieve specific dashboard
 router.get("/:dashboardId", checkToken, async (req, res) => {
   let userId = req.decoded.id;
